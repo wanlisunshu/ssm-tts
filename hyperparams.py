3858017538f0e5bba5821be9dedddc2b621f313b
WanliSun415 <672415342@qq.com>
2024-07-30 04:30:15 +0000

    hyperparams.py

--- hyperparams.py
+++ hyperparams.py
@@ -23,14 +23,14 @@ n_iter = 60
 # power = 1.5
 outputs_per_step = 1
 
-epochs = 10000
+epochs = 10
 lr = 0.001
 save_step = 2000
-image_step = 500
-batch_size = 32
+image_step = 100
+batch_size = 4
 
 cleaners='english_cleaners'
 
-data_path = './data/LJSpeech-1.1'
+data_path = '/store/store2/data/acp21ws/LJSpeech-1.1'
 checkpoint_path = './checkpoint'
 sample_path = './samples'
