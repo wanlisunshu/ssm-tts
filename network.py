9b26ccf81a34f8de134a8be57fa6f8074d66d673
Wanli <sunwanli@ra1720161920015.shef.ac.uk>
2024-08-13 07:45:50 +0100

    add padding to gradv

--- network.py
+++ network.py
@@ -220,7 +220,7 @@ class Model(nn.Module):
         mel_mask = mel_mask.view(pos_mel.shape[0], -1, 1)
 
         # shape: B,
-        gradv = t.sum(t.sum(grad1 * vectors, dim=-1), dim=-1) / mel_length
+        gradv = t.sum(t.sum(grad1 * vectors * mel_mask, dim=-1), dim=-1) / mel_length
 
         # second term in Eq. 8, shape: B,
         loss2 = t.sum(t.sum(grad1 * grad1 * mel_mask, dim=-1), dim=-1) / 2 / mel_length
